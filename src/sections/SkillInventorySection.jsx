b50adb831d85eadc9289fd7f80f15ff5c6330478
jeonghoon jeon <a1816254@gmail.com>
2026-03-09 06:17:57 +0900

    fix ag-grid core css import

--- src/sections/SkillInventorySection.jsx
+++ src/sections/SkillInventorySection.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+﻿import React, { useEffect, useMemo, useState } from "react";
 import { AgGridReact } from "ag-grid-react";
 import {
   ClientSideRowModelModule,
@@ -256,3 +256,4 @@ export default function SkillInventorySection() {
     </section>
   );
 }
+
